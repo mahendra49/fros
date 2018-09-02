9c860927b4c4eea9875824be89736aff1fd5ea51
mahendra <mahendrasuthar02@gmail.com>
2018-09-02 18:09:19 +0530

    comments done part 2 :P

--- public/js/trigger.js
+++ public/js/trigger.js
@@ -1,34 +1,23 @@
 $(document).ready(function(){
    $(".commentsubmit").on("submit",function(e){
         e.preventDefault();
-        // console.log(e);
-        console.log($(this).serialize());
-
-        console.log(e);
         var action1=e.currentTarget.action;
-        
         $.ajax({
             url:action1,
             type:'post',
             dataType:'json',
             data: $(this).serialize(),
-            success: function(data) {
-               //$(this).trigger('reset');
-               console.log(data);
-               $(".commentslist").append("<li><b>"+data.username+" </b>"+data.comment+"</li>");
-                 
+            success: (data) => {
+                $(this).closest('.commentsubmit').parent().find("ul").append("<li><b>"+data.username+" </b>"+data.comment+"</li>");
             },
             error:function(err){
                 console.log("error");
             }
-        })
+        });
         $(this).trigger("reset");
        
-        
-   });
+    });
 });
-
-
 
 
 
